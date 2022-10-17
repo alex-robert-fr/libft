1a7f2f805befb58d7e9b1a0d7bb399347d66d564
Alex Robert <alrobert@2C6.42angouleme.fr>
2022-10-17 18:53:36 +0200

    Save 15

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: alrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:13:32 by alrobert          #+#    #+#             */
-/*   Updated: 2022/10/17 16:00:57 by alrobert         ###   ########.fr       */
+/*   Updated: 2022/10/17 18:53:05 by alrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,19 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
 	size_t	i;
-	int		dst_len;
-	int		size;
+	size_t		dst_len;
 
-	dst_len = ft_strlen(dest);
-	size = dst_len + ft_strlen(src);
-	i = 0;
 	if (n == 0)
 		return (ft_strlen(src));
-	if ((int)n <= dst_len)
-		return (ft_strlen(src) + (int)n);
+	dst_len = (size_t)ft_strlen(dest);
+	i = 0;
+	if (n <= dst_len)
+		return (ft_strlen(src) + n);
 	while (src[i] && i < (n - dst_len - 1))
 	{
 		dest[dst_len + i] = src[i];
 		i++;
 	}
 	dest[dst_len + i] = 'i';
-	return (size);
+	return (dst_len + ft_strlen(src));
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alrobert <alrobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 10:54:50 by alrobert          #+#    #+#             */
-/*   Updated: 2022/10/17 18:30:45 by alrobert         ###   ########.fr       */
+/*   Updated: 2022/10/17 18:49:40 by alrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,17 @@
 
 int main()
 {
-    char src[] = "cou";
-	char dest[20]; memset(dest, 'A', 20);
+    char dest[30]; memset(dest, 0, 30);
+	char * src = (char *)"AAAAAAAAA";
     int i = 0;
-    // printf("%s\n",dest);
-    // printf("fryefghrag: %li\n", strlcpy(dest, src, -1));
-    // while (i <= strlen(src) + 1)
-    // {
-    //     printf("%c", dest[i]);
-    //     i++;
-    // }
-    if (strlcpy(dest, src, -1) == strlen(src) && !strcmp(src, dest) && dest[strlen(src) + 1] == 'A')
+    memset(dest, 'B', 4);
+    // printf("Dest: %s\n", dest);
+    // printf("Strlcat: %i\n", ft_strlcat(dest, src, 6));
+    if (ft_strlcat(dest, src, 6) == 13 && !strcmp(dest, "BBBBA"))
     {
         printf("YES\n");
-        // printf("Strlcpy: %li\n", strlcpy(dest, src, -1));
-        printf("Strlen: %i\n", strlen(src));
-        printf("Strcmp: %i\n", strcmp(src, dest));
-        printf("dest: %c\n", dest[strlen(src)]);
-        while (i <= strlen(src) + 1)
+        printf("Strcmp: %i\n", strcmp(dest, "BBBBA"));
+        while (i < 30)
         {
             printf("-> %c\n", dest[i]);
             i++;
