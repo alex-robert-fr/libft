0c0d524e627ace07710ed6ca971778de17d3caa3
Alex Robert <alrobert@2C5.42angouleme.fr>
2022-10-18 15:10:08 +0200

    Save 18

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: alrobert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:13:32 by alrobert          #+#    #+#             */
-/*   Updated: 2022/10/18 14:58:19 by alrobert         ###   ########.fr       */
+/*   Updated: 2022/10/18 15:09:01 by alrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 	if (n == 0)
 		return (ft_strlen(src));
 	dst_len = (size_t)ft_strlen(dest);
-	/*
-	printf("Destlen: %i\n", dst_len);
-	printf("Srclen: %i\n", ft_strlen(src));
-	printf("Srclen + Destlen: %i\n", ft_strlen(src) + dst_len);
-	*/
+	if (n < dst_len)
+		return (ft_strlen(src) + n);
 	i = 0;
 	while (src[i] && (i + dst_len) < (n - 1))
 	{
@@ -34,9 +31,5 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 		i++;
 	}
 	dest[i + dst_len] = '\0';
-//	printf("Pass HERE\n");
-//	printf("Srclen + Destlen: %i\n", ft_strlen(src) + dst_len);
-	if (n < dst_len)
-		return (ft_strlen(src) + n);
 	return (ft_strlen(src) + dst_len);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alrobert <alrobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 10:54:50 by alrobert          #+#    #+#             */
-/*   Updated: 2022/10/18 14:54:32 by alrobert         ###   ########.fr       */
+/*   Updated: 2022/10/18 15:03:36 by alrobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,42 @@
 
 int main()
 {
-    char dest[30]; memset(dest, 0, 30);
-	char * src = (char *)"AAAAAAAAA";
-    int i = 0;
-    memset(dest, 'B', 4);
-    int strlct = ft_strlcat(dest, src, 3);
-    if (strlct == 3 + strlen(src) && !strcmp(dest, "BBBB"))
-    {
-        printf("YES\n");
-        printf("Strlcat: %i -> len: %i\n", strlct, (3 + strlen(src)));
-        printf("Strcmp: %i\n", strcmp(dest, "B"));
-        while (i < 30)
-        {
-            printf("-> %c\n", dest[i]);
-            i++;
-        }
-    }
-    else
-    {
-        printf("NO\n");
-        printf("Strlcat: %i -> len: %i\n", strlct, (3 + strlen(src)));
-        printf("Strcmp: %i\n", strcmp(dest, "B"));
-        while (i < 30)
-        {
-            printf("-> %c\n", dest[i]);
-            i++;
-        }
-    }
+    // char dest[30]; memset(dest, 0, 30);
+	// char * src = (char *)"AAAAAAAAA";
+    // int i = 0;
+    // memset(dest, 'B', 4);
+    // int strlct = ft_strlcat(dest, src, 3);
+    // if (strlct == 3 + strlen(src) && !strcmp(dest, "BBBB"))
+    // {
+    //     printf("YES\n");
+    //     printf("Strlcat: %i -> len: %i\n", strlct, (3 + strlen(src)));
+    //     printf("Strcmp: %i\n", strcmp(dest, "B"));
+    //     while (i < 30)
+    //     {
+    //         printf("-> %c\n", dest[i]);
+    //         i++;
+    //     }
+    // }
+    // else
+    // {
+    //     printf("NO\n");
+    //     printf("Strlcat: %i -> len: %i\n", strlct, (3 + strlen(src)));
+    //     printf("Strcmp: %i\n", strcmp(dest, "B"));
+    //     while (i < 30)
+    //     {
+    //         printf("-> %c\n", dest[i]);
+    //         i++;
+    //     }
+    // }
     
+    char	*dest;
 
-    // char *str = "the cake is a lie !\0I'm hidden lol\r\n";
-    // char buff1[0xF00] = "there is no stars in the sky";
-    // char buff2[0xF00] = "there is no stars in the sky";
-    // size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
-    // size_t r1 = strlcat(buff1, str, max);
-    // size_t r2 = ft_strlcat(buff2, str, max);
-    // char s1[4] = "";
-    // char s2[4] = "";
-    // r1 = strlcat(s1, "thx to ntoniolo for this test !", 4);
-    // r2 = ft_strlcat(s2, "thx to ntoniolo for this test !", 4);
-    // printf("S1: %s\n",s1);
-    // printf("S2: %s\n",s2);
+	dest = (char *)malloc(sizeof(*dest) * 15);
+	memset(dest, 0, 15);
+	memset(dest, 'r', 6);
+
+    memset(dest, 'r', 15);
+	ft_strlcat(dest, "lorem ipsum dolor sit amet", 5);
 
     
     
