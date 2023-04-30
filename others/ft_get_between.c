07a200f234d9568baca9439d81866fb7067afe1b
0x7x00 <contact.alexrobert04@gmail.com>
2023-04-30 14:18:35 +0200

    fix ft_get_between

--- others/ft_get_between.c
+++ others/ft_get_between.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 #include "../includes/libft.h"
 
+int		count_char_for_alloc(char c, char *str)
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			result++;
+		i++;
+	}
+	return (result/2);
+}
+
+char	**alloc_get_between(char c, char *str)
+{
+	char	**array;
+	int	i;
+	int	i_word;
+	int	nb_array;
+	int	find_char;
+
+	i = 0;
+	i_word = 0;
+	nb_array = 0;
+	find_char = 0;
+	array = ft_calloc(count_char_for_alloc(c, str) + 1, sizeof(char*));
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			if (i_word)
+			{
+				array[nb_array] = ft_calloc(i_word + 1, 1);
+				nb_array++;
+			}
+			i_word = 0;
+			find_char = !find_char;
+		}
+		if (find_char && str[i] != c)
+			i_word++;
+		i++;
+	}
+	return (array);
+}
 
 // "echo" test "salut"
 // RETURN :
@@ -19,10 +66,7 @@ char	**ft_get_between(char c, char *str)
 	i_word = 0;
 	nb_array = 0;
 	find_char = 0;
-	array = ft_calloc(3, sizeof(char*));
-	array[0] = ft_calloc(6, 1);
-	array[1] = ft_calloc(6, 1);
-	array[2] = ft_calloc(1, 1);
+	array = alloc_get_between(c, str);
 	ft_putstr_fd("GET_BETWEEN\n", 1);
 	while (str[i])
 	{
