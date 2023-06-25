b2e80a3de0502211ba410a5204a3bf23aa52fd50
0x7x00 <contact.alexrobert04@gmail.com>
2023-06-25 11:21:58 +0200

    norm strcmp

--- others/ft_strcmp.c
+++ others/ft_strcmp.c
@@ -6,16 +6,16 @@
 /*   By: alex <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 11:59:08 by alex              #+#    #+#             */
-/*   Updated: 2023/06/02 11:59:11 by alex             ###   ########.fr       */
+/*   Updated: 2023/06/25 11:21:35 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(const char *s1, const char *s2)
- {
- 	int	i;
- 
- 	i = 0;
- 	while (s1[i] == s2[i] && s1[i] && s2[i])
- 		i++;
- 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
- }
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
